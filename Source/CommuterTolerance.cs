070463d35b0a1f3f12845b7b6e9cbb24f4ed8b42
garrett kelly <garrett@malechus.com>
2023-01-18 12:17:42 -0500

    Add license and readme.

--- Source/CommuterTolerance.cs
+++ Source/CommuterTolerance.cs
@@ -2,11 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ICities;
+using UnityEngine;
 
 namespace CommuterTolerance.Source
 {
-    public class CommuterTolerance
+    public class CommuterTolerance : IUserMod
     {
-        
+        public string Name
+        {
+            get { return "Commuter Tolerance"; }
+        }
+
+        public string Description
+        {
+            get { return "This mod increases the distance cims are willing to travel for work, allowing more widespread cities without causing stores and factories to close due to lack of workers."; }
+        }
     }
 }
